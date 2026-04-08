1c308f3466dbdcfb414e03b1ad20b48394d1781d
Iyad <iyadsamaha41@gmail.com>
2026-04-08 17:36:57 +0300

    save
    .

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -12,6 +12,7 @@ public class PlayerMovement : MonoBehaviour
     public KeyCode deathKey = KeyCode.R;
 
     public CharacterController2D Controller;
+    public Animator animator;
     float HorizonatalMove = 0f;
     public float RunSpeed =40f ;
     bool jump = false;
@@ -27,14 +28,21 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         HorizonatalMove = Input.GetAxisRaw("Horizontal") * RunSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(HorizonatalMove));
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("IsJumping", true);    
         }
         if (Input.GetKeyDown(deathKey))
         {
             playerHealth.Die();
         }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
     }
 
     private void FixedUpdate()
