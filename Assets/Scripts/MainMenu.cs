493476f972a738b70fb9c54bdbb29e66fff61cb4
Iyad <iyadsamaha41@gmail.com>
2026-05-20 23:33:44 +0300

    ui and stuff
    .

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,24 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
-    public void PlayGame()
+    public void GoToScene()
     {
-        SceneManager.LoadSceneAsync(1);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        SceneManager.LoadScene(1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitApp()
     {
-        
+        Application.Quit();
+        Debug.Log("Application Quit UWU");
     }
+
 }
